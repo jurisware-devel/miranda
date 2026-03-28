1947ef87bfb8c43fc4f6c3c8b58f459d53d71fb9
Jurisware Developer <devel@jurisware.com>
2026-03-28 08:14:41 -0400

    Restore optional publicationStatus on CaseItem

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,6 +1,8 @@
 import type { Schema } from "../../amplify/data/resource";
 
-export type CaseItem = Schema["Case"]["type"];
+export type CaseItem = Schema["Case"]["type"] & {
+  publicationStatus?: string | null;
+};
 export type CourtItem = Schema["Court"]["type"];
 export type TagItem = Schema["Tag"]["type"];
 export type CaseTagItem = Schema["CaseTag"]["type"];
